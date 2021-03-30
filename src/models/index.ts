7484cba8607561de88e05268610b689b27c67713
Santiago Salazar <santiago.1701913303@ucaldas.edu.co>
2021-03-29 20:55:10 -0500

    se crea el modelo project

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 export * from './city.model';
 export * from './country.model';
 
+export * from './project.model';
---
+++ src/models/project.model.ts
@@ -0,0 +1,45 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Project extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  code: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  description: string;
+
+  @property({
+    type: 'string',
+  })
+  image?: string;
+
+
+  constructor(data?: Partial<Project>) {
+    super(data);
+  }
+}
+
+export interface ProjectRelations {
+  // describe navigational properties here
+}
+
+export type ProjectWithRelations = Project & ProjectRelations;
