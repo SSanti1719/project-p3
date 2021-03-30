ecf7f3d91b08ffd2dd617fc09737a275592b4bde
Santiago Salazar <santiago.1701913303@ucaldas.edu.co>
2021-03-29 21:49:46 -0500

    Se crea los controller de Admin, Client, Payment, Project, Property

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,7 @@
 export * from './ping.controller';
 export * from './country.controller';
+export * from './project.controller';
+export * from './property.controller';
+export * from './client.controller';
+export * from './payment.controller';
+export * from './admin.controller';
